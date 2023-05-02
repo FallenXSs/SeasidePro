4ebfa919f24ac8910b67b3f0b9a99ae9a5e4d505
FallenXSs <131010263+FallenXSs@users.noreply.github.com>
2023-05-02 20:19:58 +0400

    Update followerfinder.py

--- followerfinder.py
+++ followerfinder.py
@@ -75,8 +75,8 @@ def main():
     print("")
     print(f"{Style.RESET_ALL}{get_time()} {Style.BRIGHT + Fore.GREEN}[+] {Style.BRIGHT + Fore.WHITE}Giriş yapıldı!{Style.RESET_ALL}")
     
-    if ("R35" not in logo):
-        print("Bu programı R35'den çaldım ve o kadar malım ki bunu da kaldırmayı unuttum.")
+    if ("FallenXSs" not in logo):
+        print("Bu Tool Yaqub İsmayılzadəyə Aiddir")
         exit(0)
 
     if (method == "1"):
